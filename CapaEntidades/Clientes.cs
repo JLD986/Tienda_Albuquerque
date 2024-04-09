4a074c97f75aee6d187c80ca032989fff56e63a2
Jose lisandro Ramos Domínguez <rr0263032023@unab.edu.sv>
2024-04-08 23:01:27 -0600

    alkeluyaya

---
+++ CapaEntidades/Clientes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    internal class Clientes
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ClienteId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+        [Required]
+
+        public int telefono { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string direccion { get; set; }
+
+
+    }
+}
---
+++ CapaEntidades/venta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class venta
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int VentaId { get; set; }
+        [Required]
+        public DateTime Fecha { get; set; }
+        [Required]
+        public decimal Total { get; set; }
+
+        //propiedad de navegacion
+        public List<DetalleVenta> Detalles { get; set; }
+
+        //Feliz cumpleanios pa mi
+    }
+}
