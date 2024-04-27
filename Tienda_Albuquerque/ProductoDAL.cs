105e2be76c4da4aa2c7d2a679c82bda0f14997a8
andrea-alexa <126368756+andrea-alexa@users.noreply.github.com>
2024-04-26 20:36:04 -0600

    Update ProductoDAL.cs

--- Tienda_Albuquerque/ProductoDAL.cs
+++ Tienda_Albuquerque/ProductoDAL.cs
@@ -9,5 +9,47 @@ using Tienda_Albuquerque;
 namespace CapaDatos
 {
     public class ProductoDAL
-    {
+    {ContextoDB _db;
+
+        public int Guardar(Producto producto, int id = 0, bool esActualizacion = false)
+        {
+            _db = new ContextoDB();
+
+            int resultado;
+
+            if (esActualizacion)
+            {
+                producto.ProductoId = id;
+
+                _db.Entry(producto).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+
+                resultado = producto.ProductoId;
+            }
+            else
+            {
+                _db.Productos.Add(producto);
+                _db.SaveChanges();
+
+                resultado = producto.ProductoId;
+            }
+
+            return resultado;
+        }
+
+        public List<Producto> Leer()
+        {
+            _db = new ContextoDB();
+
+            return _db.Productos.Where(p => p.Estado == true).ToList();
+        }
+
+        public Producto LeerPorId(int id)
+        {
+            _db = new ContextoDB();
+
+            return _db.Productos.Find(id);
+        }
+    }
+}
         
