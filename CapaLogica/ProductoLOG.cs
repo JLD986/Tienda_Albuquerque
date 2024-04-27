1d48e76ac55e2ba76849f292853e421a2932eb47
Antony <126409554+Antoain@users.noreply.github.com>
2024-04-26 20:25:22 -0600

    ventaLog
    porque estaba malo

--- CapaLogica/ProductoLOG.cs
+++ CapaLogica/ProductoLOG.cs
@@ -11,4 +11,28 @@ namespace CapaLogica
 {
     public class ProductoLOG
     {
+         ProductoDAL _productoDAL;
+
+        public int GuardarProducto(Producto producto)
+        {
+            _productoDAL = new ProductoDAL();
+
+            return _productoDAL.Guardar(producto);
+        }
+
+        public Producto ObtenerProductoPorId(int codigo)
+        {
+            _productoDAL = new ProductoDAL();
+
+            return _productoDAL.LeerPorId(codigo);
+        }
+
+        public List<Producto> ObtenerProductos()
+        {
+            _productoDAL = new ProductoDAL();
+
+            return _productoDAL.Leer();
+        }
+    }
+}
        
