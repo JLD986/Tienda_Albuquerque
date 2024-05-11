85023c29bf8ca3ec76d6f2cbf5469f6fbdef59a0
JLD986 <126208860+JLD986@users.noreply.github.com>
2024-05-10 23:05:01 -0600

    ClienteLog

---
+++ CapaLogica/ClienteLOG.cs
@@ -0,0 +1,21 @@
+﻿using CapaDatos;
+using CapaEntidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    public class ClienteLOG
+    {
+        ClienteDAL _ClienteDAL;
+
+        public int GuardarCliente(Cliente cliente)
+        {
+            _ClienteDAL = new ClienteDAL();
+            return _ClienteDAL.Guardar(cliente);
+        }
+    }
+}
